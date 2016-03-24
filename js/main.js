01381d0e6bebb921440461341ff85313c6f3f911
erinmilling <mill0814@algonquinlive.com>
2016-03-24 18:51:51 -0400

    created function and prevented default

--- js/main.js
+++ js/main.js
@@ -5,5 +5,11 @@ $('form').on('submit', function (e) {
   var $li = $('<li>');
 
   e.preventDefault();
+  $li.html($input.val());
+  $input.val('');
+  $list.append($li);
+});
 
+$('ul').on('click','li', function (e) {
+  $(this).toggleClass('line');
 });
